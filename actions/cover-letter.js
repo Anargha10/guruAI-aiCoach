95724062d2c605867aee73aad200fb5b0fd2805c
Anargha Bhattacharjee <anarghabhatta369@gmail.com>
2025-12-29 21:13:21 +0530

    Fix: edge runtime upgrade

--- actions/cover-letter.js
+++ actions/cover-letter.js
@@ -1,3 +1,6 @@
+export const runtime = "nodejs";
+export const preferredRegion = "auto";
+export const dynamic = "force-dynamic";
 "use server";
 
 import { db } from "@/lib/prisma";
--- actions/user.js
+++ actions/user.js
@@ -1,3 +1,7 @@
+export const runtime = "nodejs";
+export const preferredRegion = "auto";
+export const dynamic = "force-dynamic";
+
 'use server'
 
 import { db } from "@/lib/prisma";
