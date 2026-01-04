de3a0bd657d155a00eb2e5c186dc11ec571175f0
Anargha Bhattacharjee <anarghabhatta369@gmail.com>
2026-01-04 18:00:34 +0530

    Fix: ingest cron job-2

--- app/api/ingest/route.js
+++ app/api/ingest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from '@/lib/ingest/client' ;
-import { generateIndustryInsights } from "@/lib/ingest/functions";
+import { generateIndustryInsights, processSingleIndustry } from "@/lib/ingest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
